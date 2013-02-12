743507b02db883823102f3106435723200beae55
Michalis Vitos <info@michalisvitos.gr>
2013-02-12 15:53:25 +0000

    Added the Browse capability to the ProjectPickerActivity.java
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- ExCiteS Data Collector/src/uk/ac/ucl/excites/collector/ProjectPickerActivity.java
+++ ExCiteS Data Collector/src/uk/ac/ucl/excites/collector/ProjectPickerActivity.java
@@ -1,12 +1,26 @@
 package uk.ac.ucl.excites.collector;
 
+import java.io.File;
+
+import uk.ac.ucl.excites.collector.ui.filedialog.FileDialog;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProjectPickerActivity extends Activity
 {
+	// Define some variables
+	public static final int SETTINGS_REQUEST_IMPORT = 1;
+	private File browseXMLFile;
+	private EditText enterURL;
+	private Button browseButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -18,6 +32,57 @@ public class ProjectPickerActivity extends Activity
 		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 		setContentView(R.layout.activity_projectpicker);
 
+		// Get View Elements
+		enterURL = (EditText) findViewById(R.id.EnterURL);
+		browseButton = (Button) findViewById(R.id.BrowseButton);
+		
+		// Browse Button
+		browseButton.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Intent mIntent = new Intent(getBaseContext(), FileDialog.class);
+				// Start from "/sdcard"
+				mIntent.putExtra(FileDialog.START_PATH, Environment.getExternalStorageDirectory().getPath());
+
+				// can user select directories or not
+				mIntent.putExtra(FileDialog.CAN_SELECT_DIR, true);
+
+				// set file filter
+				mIntent.putExtra(FileDialog.FORMAT_FILTER, new String[] { "xml" });
+
+				startActivityForResult(mIntent, SETTINGS_REQUEST_IMPORT);
+
+			}
+		});
+
+		/*
+		 * case R.id.settings_import:
+		 * 
+		 * break;
+		 */
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if(resultCode == FileDialog.RESULT_OK)
+		{
+			switch(requestCode)
+			{
+			case SETTINGS_REQUEST_IMPORT:
+				// Get the result file path and import the settings
+				String fileSource = data.getStringExtra(FileDialog.RESULT_PATH).trim();
+				browseXMLFile = new File(fileSource);
+				enterURL.setText(fileSource);
+				enterURL.setSelection(fileSource.length());
+				break;
+			}
+		}
 	}
 
 	@Override
---
+++ ExCiteS Data Collector/src/uk/ac/ucl/excites/collector/ui/filedialog/SelectionMode.java
@@ -0,0 +1,7 @@
+package uk.ac.ucl.excites.collector.ui.filedialog;
+
+public class SelectionMode {
+	public static final int MODE_CREATE = 0;
+
+	public static final int MODE_OPEN = 1;
+}
