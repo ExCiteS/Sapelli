b625441ea762551594189ae66c6b3306b579c147
Michalis Vitos <info@michalisvitos.gr>
2013-03-06 13:18:20 +0000

    Added sound and vibration at the the end of the form
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- ExCiteS Project Model/src/uk/ac/ucl/excites/collector/project/model/Form.java
+++ ExCiteS Project Model/src/uk/ac/ucl/excites/collector/project/model/Form.java
@@ -14,66 +14,67 @@ import uk.ac.ucl.excites.storage.model.IntegerColumn;
 import uk.ac.ucl.excites.storage.model.Schema;
 
 /**
- * @author mstevens
- *
+ * @author mstevens, Michalis Vitos
+ * 
  */
 public class Form
 {
-	
-	//Statics--------------------------------------------------------
+
+	// Statics--------------------------------------------------------
 	public static final boolean END_TIME_DEFAULT = false;
-	
+
 	public static final int END_ACTION_LOOP = 0;
 	public static final int END_ACTION_EXIT = 1;
-	//public static final int END_ACTION_NEXT_FORM = 2;
+	// public static final int END_ACTION_NEXT_FORM = 2;
 	public static final int DEFAULT_END_ACTION = END_ACTION_LOOP;
-	
-	public static boolean DEFAULT_SHOW_BACK = true;
-	public static boolean DEFAULT_SHOW_CANCEL = true;
-	public static boolean DEFAULT_SHOW_FORWARD = false; //for now we never show the forward button
-	public static String DEFAULT_BUTTON_BACKGROUND_COLOR = "#FFFFFF"; //White
-	
+
+	public static final boolean DEFAULT_VIBRATE = true;
+	public static final boolean DEFAULT_SHOW_BACK = true;
+	public static final boolean DEFAULT_SHOW_CANCEL = true;
+	public static final boolean DEFAULT_SHOW_FORWARD = false; // for now we never show the forward button
+	public static final String DEFAULT_BUTTON_BACKGROUND_COLOR = "#FFFFFF"; // White
+
 	public static final String COLUMN_TIMESTAMP_START = "StartTime";
 	public static final String COLUMN_TIMESTAMP_END = "EndTime";
 	public static final String COLUMN_DEVICE_ID = "DeviceID";
-	//public static final String COLUMN_USER = "User";
+	// public static final String COLUMN_USER = "User";
 	public static final String COLUMN_SENT_AT = "SentAt";
 	public static final String COLUMN_RECEIVED_AT = "ReceivedAt";
 	public static final String COLUMN_TRANSMISSION_TYPE = "TransmissionType";
-	
-	//Dynamics-------------------------------------------------------
+
+	// Dynamics-------------------------------------------------------
 	private int schemaID;
 	private int schemaVersion;
 	private String name;
-	
-	//Fields
+
+	// Fields
 	private Field start;
 	private List<Field> fields;
 	private List<LocationField> locationFields;
 
-	//Android shortcut:
+	// Android shortcut:
 	private boolean shortcut;
-	//shortcutIcon 
+	// shortcutIcon
 
-	//Timestamps
+	// Timestamps
 	private boolean storeEndTime;
-	
-	//End action:
+
+	// End action:
 	private int endAction;
 	private boolean vibrateOnEnd;
 	private String endSoundPath;
-	
-	//Buttons:
+
+	// Buttons:
 	private boolean showBack;
 	private boolean showCancel;
 	private boolean showForward;
 	private String backgroundColor;
-	
+
 	public Form(String name, int schemaID)
 	{
 		this(name, schemaID, Schema.DEFAULT_VERSION);
 	}
-	
+
 	public Form(String name, int schemaID, int schemaVersion)
 	{
 		this.name = name;
@@ -82,39 +83,40 @@ public class Form
 		this.fields = new ArrayList<Field>();
 		this.locationFields = new ArrayList<LocationField>();
 		this.endAction = DEFAULT_END_ACTION;
+		this.vibrateOnEnd = DEFAULT_VIBRATE;
 	}
-	
+
 	public void addField(Field f)
 	{
 		fields.add(f);
 		if(f instanceof LocationField)
 			locationFields.add((LocationField) f);
 	}
-	
+
 	public Field getNextField(Field current)
 	{
 		int currentIndex = fields.indexOf(current.getRoot());
-		//Exception handling:
+		// Exception handling:
 		if(currentIndex < 0)
 			throw new IllegalArgumentException("The current field is not part of this form.");
-		//Check for jump field (possibly the one of a parent in case of Choice):
+		// Check for jump field (possibly the one of a parent in case of Choice):
 		Field next = current.getJump();
 		if(next == null)
 		{
-			//No jump is set, check for field below current one:
+			// No jump is set, check for field below current one:
 			if(currentIndex + 1 < fields.size())
-				next = fields.get(currentIndex + 1); //go to next field in the form
+				next = fields.get(currentIndex + 1); // go to next field in the form
 			else
-				next = new EndField(); //current field is the last of the form, go to end
+				next = new EndField(); // current field is the last of the form, go to end
 		}
-		return next; //use jump as next
+		return next; // use jump as next
 	}
-	
+
 	public String getName()
 	{
 		return name;
 	}
-	
+
 	public List<Field> getFields()
 	{
 		return fields;
@@ -129,13 +131,14 @@ public class Form
 	}
 
 	/**
-	 * @param start the start to set
+	 * @param start
+	 *            the start to set
 	 */
 	public void setStart(Field start)
 	{
 		this.start = start;
 	}
-	
+
 	/**
 	 * @return the showBack
 	 */
@@ -145,7 +148,8 @@ public class Form
 	}
 
 	/**
-	 * @param showBack the showBack to set
+	 * @param showBack
+	 *            the showBack to set
 	 */
 	public void setShowBack(boolean showBack)
 	{
@@ -161,7 +165,8 @@ public class Form
 	}
 
 	/**
-	 * @param showCancel the showCancel to set
+	 * @param showCancel
+	 *            the showCancel to set
 	 */
 	public void setShowCancel(boolean showCancel)
 	{
@@ -177,7 +182,8 @@ public class Form
 	}
 
 	/**
-	 * @param showForward the showForward to set
+	 * @param showForward
+	 *            the showForward to set
 	 */
 	public void setShowForward(boolean showForward)
 	{
@@ -188,7 +194,7 @@ public class Form
 	{
 		return locationFields;
 	}
-	
+
 	public List<LocationField> getLocationFields(boolean onlyStartWithForm)
 	{
 		if(onlyStartWithForm)
@@ -201,7 +207,7 @@ public class Form
 		}
 		return getLocationFields();
 	}
-	
+
 	/**
 	 * @return the storeEndTime
 	 */
@@ -211,7 +217,8 @@ public class Form
 	}
 
 	/**
-	 * @param storeEndTime the storeEndTime to set
+	 * @param storeEndTime
+	 *            the storeEndTime to set
 	 */
 	public void setStoreEndTime(boolean storeEndTime)
 	{
@@ -225,15 +232,16 @@ public class Form
 	{
 		return endAction;
 	}
-		
+
 	/**
-	 * @param endAction the endAction to set
+	 * @param endAction
+	 *            the endAction to set
 	 */
 	public void setEndAction(int endAction)
 	{
 		this.endAction = endAction;
 	}
-	
+
 	/**
 	 * @return the vibrateOnEnd
 	 */
@@ -243,65 +251,82 @@ public class Form
 	}
 
 	/**
+	 * @param vibrateOnEnd
+	 * 
+	 */
+	public void setVibrateOnEnd(boolean vibrateOnEnd)
+	{
+		this.vibrateOnEnd = vibrateOnEnd;
+	}
+
+	/**
 	 * @return the endSoundPath
 	 */
 	public String getEndSoundPath()
 	{
 		return endSoundPath;
 	}
-	
+
+	/**
+	 * Set the end sound
+	 * @param endSoundPath
+	 */
+	public void setEndSoundPath(String endSoundPath)
+	{
+		this.endSoundPath = endSoundPath;
+	}
+
 	public String getBackImagePath()
 	{
-		return null; //TODO
+		return null; // TODO
 	}
-	
+
 	public String getCancelImagePath()
 	{
-		return null; //TODO
+		return null; // TODO
 	}
-	
+
 	public String getForwardImagePath()
 	{
-		return null; //TODO
+		return null; // TODO
 	}
 
 	public Schema getSchema(DataAccess dao)
-	{	
+	{
 		Schema schema = dao.retrieveSchema(schemaID, schemaVersion);
 		if(schema == null)
 		{
 			schema = new Schema(schemaID, schemaVersion, name);
-			//Internal-use columns:
+			// Internal-use columns:
 			// Timestamp column(s):
 			schema.addColumn(DateTimeColumn.Century21NoMS(COLUMN_TIMESTAMP_START, false));
 			if(storeEndTime)
-				schema.addColumn(DateTimeColumn.Century21NoMS(COLUMN_TIMESTAMP_END, false));			
+				schema.addColumn(DateTimeColumn.Century21NoMS(COLUMN_TIMESTAMP_END, false));
 			// Device ID column:
 			schema.addColumn(new IntegerColumn(COLUMN_DEVICE_ID, false, false, 32));
 			// Transmission information columns:
-			
-			//Columns for user-defined fields:
+
+			// Columns for user-defined fields:
 			for(Field f : fields)
 				if(!f.isNoColumn())
 					schema.addColumn(f.createColumn());
-			//Seal & store the schema:
+			// Seal & store the schema:
 			schema.seal();
-			dao.store(schema); //!!!
+			dao.store(schema); // !!!
 		}
 		return schema;
 	}
-	
+
 	public FormEntry newEntry(DataAccess dao, long deviceID)
 	{
 		FormEntry entry = new FormEntry(this, dao);
-		
-		//TODO Set current time as start timestamp
+
+		// TODO Set current time as start timestamp
 		DateTime dt;
-		
-		//TODO Set deviceID
-		
-		
+
+		// TODO Set deviceID
+
 		return entry;
 	}
-	
+
 }
