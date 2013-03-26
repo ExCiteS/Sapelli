5d3b8ffda5f0e6ac23c6f28d8a8d290428ebd350
Michalis Vitos <info@michalisvitos.gr>
2013-03-26 11:38:54 +0000

    Added a BootReceiver to start the Relay on phone start up
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

---
+++ ExCiteS Data Relay/src/uk/ac/ucl/excites/relay/BootReceiver.java
@@ -0,0 +1,31 @@
+package uk.ac.ucl.excites.relay;
+
+import java.util.Calendar;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver
+{
+
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+
+		Intent service = new Intent(context, BackgroundService.class);
+		PendingIntent pendingIntent = PendingIntent.getService(context, 0, service, 0);
+
+		// Set up a calendar 2 minutes from now
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(System.currentTimeMillis());
+		calendar.add(Calendar.MINUTE, 2);
+
+		// Schedule the alarm!
+		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+	}
+
+}
