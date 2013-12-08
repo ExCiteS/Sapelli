8028022760275d935749fb86cc88d5644da84366
Matthias Stevens <matthias.stevens@gmail.com>
2013-12-08 17:45:12 +0000

    Added splitLong & mergeLong
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

--- ExCiteS Data Collection Library/src/uk/ac/ucl/excites/util/BinaryHelpers.java
+++ ExCiteS Data Collection Library/src/uk/ac/ucl/excites/util/BinaryHelpers.java
@@ -58,5 +58,46 @@ public final class BinaryHelpers
 	{
 		return (bits + 7) / 8;
 	}
+	
+	/**
+	 * Splits a long integer in the requested number equally-sized parts, each consisting of the specified number of bits.
+	 * Note that if the total number of allowed bits (numberOfParts * bitsPerPart) is insufficient to represent the entire
+	 * number data loss will occur at the most significant bits side.
+	 * 
+	 * In the returned array the more significant parts come first.
+	 * 
+	 * @param number
+	 * @param numberOfParts
+	 * @param bitsPerPart
+	 * @return
+	 */
+	public static long[] splitLong(long number, int numberOfParts, int bitsPerPart)
+	{
+		long[] parts = new long[numberOfParts];
+		for(int i = numberOfParts - 1; i >= 0; i--)
+		{
+			parts[i] = number % (1 << bitsPerPart);
+			number >>= bitsPerPart;
+		}
+		return parts;
+	}
+	
+	/**
+	 * Merges the provided long integers into one larger one. Each part will take up the specified number of bits and parts
+	 * are to be provided in most-significant-first order.
+	 * If any of the parts requires more than the allowed number of bits to be represented data loss will occur due to some
+	 * of the most significant bits of the part (not the merged number) being discarded.
+	 * 
+	 * @param parts
+	 * @param bitsPerPart
+	 * @return
+	 */
+	public static long mergeLong(long[] parts, int bitsPerPart)
+	{
+		long number = 0;
+		for(int i = 0; i < parts.length; i++)
+			number += (parts[parts.length - 1 - i] % (1 << bitsPerPart + 1)) << (bitsPerPart * i);
+		return number;
+	}
 
 }
