805f276192419f868e7e01ec4c0eae473536ee8a
Matthias Stevens <matthias.stevens@gmail.com>
2014-03-15 23:40:34 +0100

    equals() methods to compare Sets and Maps
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

--- Library/src/uk/ac/ucl/excites/sapelli/util/CollectionUtils.java
+++ Library/src/uk/ac/ucl/excites/sapelli/util/CollectionUtils.java
@@ -1,6 +1,8 @@
 package uk.ac.ucl.excites.sapelli.util;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author mstevens
@@ -70,6 +72,36 @@ public final class CollectionUtils
 		if(delimit)
 			bldr.append(']');
 		return bldr.toString();
+	}
+	
+	/**
+	 * Equality check of set contents
+	 * 
+	 * @param set1
+	 * @param set2
+	 * @return
+	 */
+	public static <O> boolean equals(Set<O> set1, Set<O> set2)
+	{
+		return set1.size() == set2.size() && set1.containsAll(set2);
+	}
+	
+	/**
+	 * Equality check of map contents
+	 * Note: values are only compared by pointers (==)
+	 * 
+	 * @param map1
+	 * @param map2
+	 * @return
+	 */
+	public static <K, V> boolean equals(Map<K, V> map1, Map<K, V> map2)
+	{
+		if(map1.size() != map2.size())
+			return false;
+		for(K key : map1.keySet())
+			if(map1.get(key) != map2.get(key))
+				return false;
+		return true;
 	}
 	
 }
