84884224ae53b0e8bdb7641289ac7ff223299f14
Michalis Vitos <info@michalisvitos.gr>
2014-03-19 17:34:00 +0000

    Work on the SelectionQuery
    Signed-off-by: Matthias Stevens

--- Library/src/uk/ac/ucl/excites/sapelli/storage/queries/SelectionQuery.java
+++ Library/src/uk/ac/ucl/excites/sapelli/storage/queries/SelectionQuery.java
@@ -3,6 +3,9 @@
  */
 package uk.ac.ucl.excites.sapelli.storage.queries;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import uk.ac.ucl.excites.sapelli.storage.model.Record;
 
 /**
@@ -12,6 +15,15 @@ import uk.ac.ucl.excites.sapelli.storage.model.Record;
 public abstract class SelectionQuery
 {
 	
+	public List<Record> filter(List<Record> records)
+	{
+		List<Record> result = new ArrayList<Record>();
+		for(Record r : records)
+			if(select(r))
+				result.add(r);
+		return result;
+	}
+
 	public boolean select(Record record)
 	{
 		return record != null & isValid(record);
