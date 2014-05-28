656c6f6439236aca15265a6a8be2eadd6dc8802d
Michalis Vitos <info@michalisvitos.gr>
2014-05-28 16:53:04 +0100

    Added project hash code parameter to the SapelliAlarmManager, the Alarm calls the service and passes the project's hash for which the Alarm is triggered
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- CollectorAndroid/src/uk/ac/ucl/excites/sapelli/sender/util/SapelliAlarmManager.java
+++ CollectorAndroid/src/uk/ac/ucl/excites/sapelli/sender/util/SapelliAlarmManager.java
@@ -13,51 +13,54 @@ import android.content.Intent;
 public class SapelliAlarmManager
 {
 	public static final int SERVICE_REQUEST_CODE = 0;
+	public static final String PROJECT_HASH_CODE = "PROJECT_HASH_CODE";
 
-	public SapelliAlarmManager()
-	{
-	}
+	public SapelliAlarmManager() {}
 
 	/**
-	 * Set up an Alarm that calls the {@link DataSenderService}, initially after a minute and then every <code>intervalMillis</code>
+	 * Set up an Alarm for a project that calls the {@link DataSenderService}, initially after a minute and then every <code>intervalMillis</code>
 	 * 
 	 * @param context
 	 * @param intervalMillis
+	 * @param projectHashCode
 	 */
-	public static void setAlarm(Context context, int intervalMillis)
+	public static void setAlarm(Context context, int intervalMillis, int projectHashCode)
 	{
-		setAlarm(context, 60 * 1000, intervalMillis);
+		setAlarm(context, 60 * 1000, intervalMillis, projectHashCode);
 	}
 
 	/**
-	 * Set up an Alarm that calls the {@link DataSenderService}, initially after <code>triggerAtMillis</code> and then every <code>intervalMillis</code>
+	 * Set up an Alarm for a project that calls the {@link DataSenderService}, initially after <code>triggerAtMillis</code> and then every
+	 * <code>intervalMillis</code>
 	 * 
 	 * @param context
 	 * @param triggerAtMillis
 	 * @param intervalMillis
+	 * @param projectHashCode
 	 */
-	public static void setAlarm(Context context, int triggerAtMillis, int intervalMillis)
+	public static void setAlarm(Context context, int triggerAtMillis, int intervalMillis, int projectHashCode)
 	{
 		// Create Alarm Manager
 		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
 		// Setup the alarm to be triggered every intervalMillis
-		alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtMillis, intervalMillis, getAlarmIntent(context));
+		alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtMillis, intervalMillis, getAlarmIntent(context, projectHashCode));
 	}
 
-	private static PendingIntent getAlarmIntent(Context context)
+	private static PendingIntent getAlarmIntent(Context context, int projectHashCode)
 	{
 		// Create the PendingIntent for the DataSenderService
 		Intent service = new Intent(context, DataSenderService.class);
+		service.putExtra(PROJECT_HASH_CODE, projectHashCode);
 		PendingIntent alarmIntent = PendingIntent.getService(context, SERVICE_REQUEST_CODE, service, 0);
 		return alarmIntent;
 	}
 
-	public static void cancelAlarm(Context context)
+	public static void cancelAlarm(Context context, int projectHashCode)
 	{
 		// Create Alarm Manager
 		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
-		alarmManager.cancel(getAlarmIntent(context));
+		alarmManager.cancel(getAlarmIntent(context, projectHashCode));
 	}
 }
