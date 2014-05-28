96ea65dbfad5782d1ae535508d59f80f843c1655
Michalis Vitos <info@michalisvitos.gr>
2014-05-28 18:06:08 +0100

    Debug info in DataSenderService
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- CollectorAndroid/src/uk/ac/ucl/excites/sapelli/sender/DataSenderService.java
+++ CollectorAndroid/src/uk/ac/ucl/excites/sapelli/sender/DataSenderService.java
@@ -1,22 +1,26 @@
 package uk.ac.ucl.excites.sapelli.sender;
 
+import uk.ac.ucl.excites.sapelli.sender.util.SapelliAlarmManager;
+import uk.ac.ucl.excites.sapelli.util.Debug;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 
 /**
- * A service that checks about network connectivity and tries to send the data Also the service tries to upload file attachments to Dropbox
- * 
  * @author Michalis Vitos
- * 
  */
 public class DataSenderService extends Service
 {
-
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId)
 	{
-		return startId;
+		Bundle extras = intent.getExtras();
+		final int projectHash = extras.getInt(SapelliAlarmManager.PROJECT_HASH_CODE);
+
+		Debug.d("Call to service: " + startId + " for project: " + projectHash);
+
+		return Service.START_NOT_STICKY;
 	}
 
 	@Override
