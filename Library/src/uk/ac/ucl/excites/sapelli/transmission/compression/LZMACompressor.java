bea407f67ca2a3edbfb5e953667e8a86afaa025f
Matthias Stevens <matthias.stevens@gmail.com>
2014-07-02 16:21:28 +0100

    Added license disclaimer to all Java files in Sapelli Library
    TODO: do the same for Android app code
    
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

--- Library/src/uk/ac/ucl/excites/sapelli/transmission/compression/LZMACompressor.java
+++ Library/src/uk/ac/ucl/excites/sapelli/transmission/compression/LZMACompressor.java
@@ -1,3 +1,21 @@
+/**
+ * Sapelli data collection platform: http://sapelli.org
+ * 
+ * Copyright 2012-2014 University College London - ExCiteS group
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
 package uk.ac.ucl.excites.sapelli.transmission.compression;
 
 import java.io.BufferedInputStream;
@@ -15,7 +33,7 @@ import uk.ac.ucl.excites.sapelli.transmission.compression.CompressorFactory.Comp
 
 /**
  * LZMA compressor.<br/>
- * Based on the lzma-java library by Julien Ponge (jponge) et al. (Apache License 2.0).
+ * Uses the lzma-java library by Julien Ponge (jponge) et al. (Apache License 2.0).
  * 
  * We do not use the LZMAOutputStream and LZMAInputStream classes (also provided by the lzma-java library)
  * because they write a header (containing encoder settings and uncompressed data length) which we do not
