e7ee299128e8563d5abfeb83d583a16d8cf166da
Michalis Vitos <info@michalisvitos.gr>
2014-07-04 16:35:20 +0100

    Changed the way PageAnimation works to also animate the previous view and be compatible with older versions of Android
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- CollectorAndroid/src/uk/ac/ucl/excites/sapelli/collector/ui/CollectorView.java
+++ CollectorAndroid/src/uk/ac/ucl/excites/sapelli/collector/ui/CollectorView.java
@@ -17,6 +17,7 @@ import uk.ac.ucl.excites.sapelli.collector.model.fields.OrientationField;
 import uk.ac.ucl.excites.sapelli.collector.model.fields.Page;
 import uk.ac.ucl.excites.sapelli.collector.model.fields.PhotoField;
 import uk.ac.ucl.excites.sapelli.collector.model.fields.TextBoxField;
+import uk.ac.ucl.excites.sapelli.collector.ui.animation.PageAnimator;
 import uk.ac.ucl.excites.sapelli.collector.ui.fields.AndroidAudioUI;
 import uk.ac.ucl.excites.sapelli.collector.ui.fields.AndroidButtonUI;
 import uk.ac.ucl.excites.sapelli.collector.ui.fields.AndroidCheckBoxUI;
@@ -30,9 +31,6 @@ import uk.ac.ucl.excites.sapelli.collector.ui.fields.AndroidPhotoUI;
 import uk.ac.ucl.excites.sapelli.collector.ui.fields.AndroidTextBoxUI;
 import uk.ac.ucl.excites.sapelli.collector.ui.fields.FieldUI;
 import uk.ac.ucl.excites.sapelli.collector.util.ScreenMetrics;
-import android.animation.Animator;
-import android.animation.LayoutTransition;
-import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
@@ -50,9 +48,9 @@ import android.widget.LinearLayout;
 @SuppressLint("ViewConstructor")
 public class CollectorView extends LinearLayout implements CollectorUI<View, CollectorView>
 {
-	
+
 	static private final String TAG = "CollectorView";
-	
+
 	static private final int BUTTONS_VIEW_ID = 0;
 	static private final int FIELD_VIEW_ID = 1;
 	
@@ -61,36 +59,39 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 	// Spacing (in dip) between UI elements:
 	static public final float SPACING_DIP = 8.0f;
 	static public final float PADDING_DIP = 2.0f;
-	
+
 	static public final int COLOR_SEMI_TRANSPARENT_GRAY = Color.parseColor("#80777777");
 	static public final int COLOR_GRAY = Color.parseColor("#B9B9B9");
-	
+
+	// PageAnimation duration
+	static public final int PAGE_ANIMATION_DURATION = 800;
+
 	private CollectorActivity activity;
 	private CollectorController controller;
-	
+
 	// UI elements:
 	private AndroidControlsUI controlsUI;
 	private FieldUI<?, View, CollectorView> fieldUI;
 	private View fieldUIView = null;
 	private HashMap<Field, FieldUI<?, View, CollectorView>> fieldUICache;
-	
+
 	// Input manager:
 	private InputMethodManager imm;
-	
+
 	public CollectorView(CollectorActivity activity)
 	{
 		super(activity);
 		this.activity = activity;
 		this.fieldUICache = new HashMap<Field, FieldUI<?, View, CollectorView>>();
-		
+
 		this.imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
-		
+
 		// Root layout (= this):
 		this.setOrientation(LinearLayout.VERTICAL);
 		this.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 		this.setBackgroundColor(Color.BLACK);
 	}
-	
+
 	/**
 	 * Sets the controller and (re)initialises the view
 	 * 
@@ -99,10 +100,10 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 	public void initialise(CollectorController controller)
 	{
 		this.controller = controller;
-		
+
 		// Clear cache:
 		fieldUICache.clear();
-		
+
 		// Set-up controlsView:
 		controlsUI = new AndroidControlsUI(controller, this);
 		View controlsView = controlsUI.getPlatformView();
@@ -120,20 +121,17 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 	{
 		if(controller == null)
 			throw new IllegalStateException("CollectorView is not initialised.");
-		
-		// Clear the LayoutTransition:
-		this.setLayoutTransition(null);
 
 		// avoid layout shift (known Android bug when using full screen)
 		activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
+
 		// Briefly disable the controls:
 		controlsUI.disable();
-		
+
 		// Get or create fieldUI for field...
 		FieldUI<?, View, CollectorView> newFieldUI = fieldUICache.get(field); // try to recycle cached fieldUI
 		if(newFieldUI == null)
-		{	// no cached fieldUI for this field...
+		{ // no cached fieldUI for this field...
 			newFieldUI = field.createUI(this); // create new fieldUI for field
 			if(newFieldUI == null) // just in case
 				throw new IllegalStateException("Could not construct UI for field \"" + field.getID() + "\".");
@@ -143,16 +141,16 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 		// Hide current fieldUI, if there is one, it is not the same as the new one (i.e. it does probably not represent the same field), and it is currently shown...
 		if(fieldUI != null && newFieldUI != fieldUI && fieldUI.isFieldShown())
 			fieldUI.hideField(); // mark field as not shown, and execute cancel behaviour (e.g. stop audio recording, close camera, ...)
-		
+
 		// newFieldUI become the current fieldUI:
 		fieldUI = newFieldUI;
 
 		// Update the controls:
 		controlsUI.update(fieldUI);
-		
+
 		// Get the actual (updated) View instance:
 		View newFieldUIView = fieldUI.showField(false, controller.getCurrentRecord());
-		
+
 		// Replace current view:
 		if(newFieldUIView != fieldUIView)
 		{
@@ -160,33 +158,21 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 			final PageAnimation pageAnimation = controller.getCurrentForm().getPageAnimation();
 			if(pageAnimation != null)
 			{
-				Animator animation;
 				switch(pageAnimation)
 				{
 				case HORIZONTAL:
 					// Check whether it is a backwards or forwards direction and create Right or Left animation:
 					if(controller.isGoBack())
-						// Left:
-						animation = ObjectAnimator.ofFloat(null, "translationX", -ScreenMetrics.GetScreenWidth(activity), 0);
-					else
 						// Right:
-						animation = ObjectAnimator.ofFloat(null, "translationX", ScreenMetrics.GetScreenWidth(activity), 0);
-					
-					// Run the animation:
-					animatePage(animation);
+						PageAnimator.slideRight(activity, fieldUIView, newFieldUIView, PAGE_ANIMATION_DURATION);
+					else
+						// Left:
+						PageAnimator.slideLeft(activity, fieldUIView, newFieldUIView, PAGE_ANIMATION_DURATION);
 					break;
 
 				case VERTICAL:
 					// Check whether it is a backwards or forwards direction and create Up or Down animation:
-					if(controller.isGoBack())
-						// Up:
-						animation = ObjectAnimator.ofFloat(null, "translationY", ScreenMetrics.GetScreenHeight(activity), 0);
-					else
-						// Down:
-						animation = ObjectAnimator.ofFloat(null, "translationY", -ScreenMetrics.GetScreenHeight(activity), 0);
-
-					// Run the animation:
-					animatePage(animation);
+					// TODO
 					break;
 
 				default:
@@ -207,29 +193,19 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 			// New becomes current:
 			fieldUIView = newFieldUIView;
 		}
-		
+
 		// Set focus:
 		fieldUI.claimFocus();
-		
+
 		// Re-enable the controls:
 		controlsUI.enable();
-	}
-	
-	@SuppressLint("NewApi")
-	private void animatePage(Animator animation)
-	{
-		LayoutTransition layoutTransition = new LayoutTransition();
-		layoutTransition.setAnimator(LayoutTransition.APPEARING, animation);
-		layoutTransition.setDuration(LayoutTransition.APPEARING, 400);
-		layoutTransition.setStartDelay(LayoutTransition.APPEARING, 0);
-		this.setLayoutTransition(layoutTransition);
 	}
 
 	public CollectorActivity getActivity()
 	{
 		return activity;
 	}
-	
+
 	@Override
 	public FieldUI<?, View, CollectorView> getCurrentFieldUI()
 	{
@@ -253,13 +229,13 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 	{
 		return new AndroidAudioUI(af, controller, this);
 	}
-	
+
 	@Override
 	public AndroidLocationUI createLocationUI(LocationField lf)
 	{
 		return new AndroidLocationUI(lf, controller, this);
 	}
-	
+
 	@Override
 	public AndroidOrientationUI createOrientationUI(OrientationField of)
 	{
@@ -271,49 +247,49 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 	{
 		return new AndroidLabelUI(lf, controller, this);
 	}
-	
+
 	@Override
 	public AndroidButtonUI createButtonUI(ButtonField bf)
 	{
 		return new AndroidButtonUI(bf, controller, this);
 	}
-	
+
 	@Override
 	public AndroidTextBoxUI createTextFieldUI(TextBoxField tf)
 	{
 		return new AndroidTextBoxUI(tf, controller, this);
 	}
-	
+
 	@Override
 	public AndroidCheckBoxUI createCheckBoxFieldUI(CheckBoxField cbf)
 	{
 		return new AndroidCheckBoxUI(cbf, controller, this);
 	}
-	
+
 	@Override
 	public AndroidMultiListUI createMultiListUI(MultiListField mlf)
 	{
 		return new AndroidMultiListUI(mlf, controller, this);
 	}
-	
+
 	@Override
 	public AndroidPageUI createPageUI(Page pg)
 	{
 		return new AndroidPageUI(pg, controller, this);
 	}
-	
+
 	@Override
 	public AndroidControlsUI createControlsUI()
 	{
 		return new AndroidControlsUI(controller, this);
 	}
-	
+
 	public void cancelCurrentField()
 	{
 		if(fieldUI != null)
 			fieldUI.hideField();
 	}
-	
+
 	/**
 	 * Removes the view corresponding to the given field from the cache, ensuring a new view will be constructed next time the field is entered
 	 * 
@@ -323,7 +299,7 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 	{
 		fieldUICache.remove(field);
 	}
-	
+
 	/**
 	 * Propagate enable/disable to children
 	 * 
@@ -337,7 +313,7 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 		if(fieldUIView != null)
 			fieldUIView.setEnabled(enabled);
 	}
-	
+
 	/*
 	 * UI element dimensions examples:
 	 * 
@@ -385,55 +361,55 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 	{
 		return ScreenMetrics.ConvertDipToPx(activity, SPACING_DIP);
 	}
-	
+
 	public int convertDipToPx(float dip)
 	{
 		return ScreenMetrics.ConvertDipToPx(activity, dip);
 	}
-	
+
 	@Override
 	public int getScreenWidthPx()
 	{
 		return ScreenMetrics.GetScreenWidth(activity);
 	}
-	
+
 	@Override
 	public int getScreenHeightPx()
 	{
 		return ScreenMetrics.GetScreenHeight(activity);
 	}
-	
-	//TODO pull all getFieldUI*Px methods up to CollectorUI
+
+	// TODO pull all getFieldUI*Px methods up to CollectorUI
 	public int getFieldUIWidthPx()
 	{
 		return getScreenWidthPx();
 	}
-	
+
 	public int getFieldUIPartWidthPx(int numCols)
 	{
 		return getFieldUIPartWidthPx(getFieldUIWidthPx(), numCols);
 	}
-	
+
 	public int getFieldUIPartWidthPx(int availableWidth, int numCols)
 	{
 		return Math.max((availableWidth - ((numCols - 1) * getSpacingPx())) / numCols, 0); // We use Math(x, 0) to avoid negative pixel counts
 	}
-	
+
 	public int getFieldUIHeightPx()
 	{
 		return getScreenHeightPx() - controlsUI.getCurrentHeightPx();
 	}
-	
+
 	public int getFieldUIPartHeightPx(int numRows)
 	{
 		return getFieldUIPartHeightPx(getFieldUIHeightPx(), numRows);
 	}
-	
+
 	public int getFieldUIPartHeightPx(int availableHeight, int numRows)
 	{
 		return Math.max((availableHeight - ((numRows - 1) * getSpacingPx())) / numRows, 0); // We use Math(y, 0) to avoid negative pixel counts
 	}
-	
+
 	/**
 	 * Show software keyboard for input on the given view.
 	 * 
@@ -452,11 +428,11 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 			Log.e(TAG, "Exception upon trying to show keyboard.", e);
 		}
 	}
-	
+
 	/**
 	 * Hide the software keyobard is currently shown.
 	 * 
-	 * TODO check what happens on devices with an (exposed) hardware keyboard 
+	 * TODO check what happens on devices with an (exposed) hardware keyboard
 	 */
 	public void hideKeyboard()
 	{
@@ -477,7 +453,7 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 	{
 		return controlsUI;
 	}
-	
+
 	/**
 	 * Makes the currently focused view lose its focus
 	 */
@@ -487,5 +463,5 @@ public class CollectorView extends LinearLayout implements CollectorUI<View, Col
 		if(focusedView != null)
 			focusedView.clearFocus();
 	}
-	
+
 }
---
+++ CollectorAndroid/src/uk/ac/ucl/excites/sapelli/collector/ui/animation/PageAnimator.java
@@ -0,0 +1,80 @@
+package uk.ac.ucl.excites.sapelli.collector.ui.animation;
+
+import uk.ac.ucl.excites.sapelli.collector.util.ScreenMetrics;
+import android.content.Context;
+import android.view.View;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
+
+public class PageAnimator
+{
+	public PageAnimator()
+	{
+	}
+
+	public static void slideRight(Context context, View previousView, View nextView, int duration)
+	{
+		int screenWidth = ScreenMetrics.GetScreenWidth(context);
+
+		// Slide previousView:
+		if(previousView != null)
+		{
+			TranslateAnimation previousAnimation = new TranslateAnimation(0, screenWidth, 0, 0);
+			previousAnimation.setDuration(duration);
+
+			// Create an animation set
+			AnimationSet previousSet = new AnimationSet(true);
+			previousSet.addAnimation(previousAnimation);
+
+			previousView.startAnimation(previousSet);
+		}
+
+		// Slide nextView:
+		TranslateAnimation nextAnimation = new TranslateAnimation(-screenWidth, 0, 0, 0);
+		nextAnimation.setDuration(duration);
+		// Give a minimal offset to separate the two animations
+		nextAnimation.setStartOffset(1);
+
+		// Create an animation set
+		AnimationSet nextSet = new AnimationSet(true);
+		nextSet.addAnimation(nextAnimation);
+		nextView.startAnimation(nextSet);
+	}
+
+	/**
+	 * Slide on the left the previous and the next views
+	 * 
+	 * @param context
+	 * @param previousView
+	 * @param nextView
+	 * @param duration
+	 */
+	public static void slideLeft(Context context, View previousView, View nextView, int duration)
+	{
+		int screenWidth = ScreenMetrics.GetScreenWidth(context);
+
+		// Slide previousView:
+		if(previousView != null)
+		{
+			TranslateAnimation previousAnimation = new TranslateAnimation(0, -screenWidth, 0, 0);
+			previousAnimation.setDuration(duration);
+
+			// Create an animation set
+			AnimationSet previousSet = new AnimationSet(true);
+			previousSet.addAnimation(previousAnimation);
+
+			previousView.startAnimation(previousSet);
+		}
+
+		// Slide nextView:
+		TranslateAnimation nextAnimation = new TranslateAnimation(screenWidth, 0, 0, 0);
+		nextAnimation.setDuration(duration);
+		// Give a minimal offset to separate the two animations
+		nextAnimation.setStartOffset(10);
+
+		// Create an animation set
+		AnimationSet nextSet = new AnimationSet(true);
+		nextSet.addAnimation(nextAnimation);
+		nextView.startAnimation(nextSet);
+	}
+}
