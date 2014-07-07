c843ddc9ad1a1d1afc2de66d03e58662a60855a2
Michalis Vitos <info@michalisvitos.gr>
2014-07-07 17:40:45 +0100

    Using the Android TTS (Text-To-Speech) Engine to speak the description (value) of a ChoiceField on a longClick
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- CollectorAndroid/src/uk/ac/ucl/excites/sapelli/collector/ui/fields/AndroidChoiceUI.java
+++ CollectorAndroid/src/uk/ac/ucl/excites/sapelli/collector/ui/fields/AndroidChoiceUI.java
@@ -19,6 +19,7 @@
 package uk.ac.ucl.excites.sapelli.collector.ui.fields;
 
 import java.io.File;
+import java.util.Locale;
 
 import uk.ac.ucl.excites.sapelli.collector.control.CollectorController;
 import uk.ac.ucl.excites.sapelli.collector.control.Controller.LeaveRule;
@@ -39,8 +40,10 @@ import uk.ac.ucl.excites.sapelli.collector.util.ColourHelpers;
 import uk.ac.ucl.excites.sapelli.collector.util.ScreenMetrics;
 import uk.ac.ucl.excites.sapelli.shared.io.FileHelpers;
 import uk.ac.ucl.excites.sapelli.storage.model.Record;
+import uk.ac.ucl.excites.sapelli.util.Debug;
 import android.content.Context;
 import android.graphics.Color;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
@@ -60,6 +63,8 @@ public class AndroidChoiceUI extends ChoiceUI<View, CollectorView>
 	static public final float PAGE_CHOSEN_ITEM_MARGIN_DIP = 1.0f; // same margin all round
 	static public final float CROSS_THICKNESS = 0.02f;
 	
+	static private TextToSpeech tts;
+
 	private PageView pageView;
 	private ChoiceView choiceView;
 
@@ -126,6 +131,58 @@ public class AndroidChoiceUI extends ChoiceUI<View, CollectorView>
 			action.run(); //perform task now (animation is disabled)	
 	}
 	
+	protected boolean onChildLongClick(Context context, final ChoiceField child, View childView)
+	{
+		// Ignore click if child is disabled:
+		if(!isFieldShown() && !controller.isFieldEnabled(child))
+			return false;
+
+		// TODO check whether there is an audio file for the given ChoiceField and use that instead of the TTS
+
+		// Use the Android TTS (Text-To-Speech) Engine
+		if(tts == null)
+			tts = new TextToSpeech(context, new TextToSpeech.OnInitListener()
+			{
+				@Override
+				public void onInit(int status)
+				{
+					if(status == TextToSpeech.SUCCESS)
+					{
+						int result = tts.setLanguage(Locale.UK);
+						if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED)
+						{
+							Debug.d("This Language is not supported");
+						}
+						else
+						{
+							textToSpeech(child);
+						}
+					}
+					else
+						Debug.d("Initilization Failed!");
+				}
+			});
+		else
+			textToSpeech(child);
+
+		return true;
+	}
+
+	/**
+	 * Use the Android TTS (Text-To-Speech) Engine to speak the description (value) of a ChoiceField
+	 * 
+	 * @param child
+	 */
+	private void textToSpeech(final ChoiceField child)
+	{
+		String childDescription = child.getValue();
+
+		if(childDescription == null || "".equals(childDescription))
+			tts.speak("Content not available", TextToSpeech.QUEUE_FLUSH, null);
+		else
+			tts.speak(childDescription, TextToSpeech.QUEUE_FLUSH, null);
+	}
+
 	/**
 	 * To be overridden by AndroidICSChoiceUI
 	 * 
@@ -309,6 +366,9 @@ public class AndroidChoiceUI extends ChoiceUI<View, CollectorView>
 		@Override
 		public boolean onItemLongClick(AdapterView<?> parent, View v, int position, long id)
 		{
+			if(isEnabled())
+				return onChildLongClick(getContext(), field.getChildren().get(position) /* pass the chosen child */, v);
+
 			return false;
 		}
 
