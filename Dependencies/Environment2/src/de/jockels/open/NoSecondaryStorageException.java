1e90dc90ee71d6db138d2db7bf2418b31feafd0b
Matthias Stevens <matthias.stevens@gmail.com>
2014-10-02 11:36:55 +0100

    Removed Environment2 dependency
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

--- Dependencies/Environment2/src/de/jockels/open/NoSecondaryStorageException.java
+++
@@ -1,8 +0,0 @@
-package de.jockels.open;
-
-import java.io.IOException;
-
-@SuppressWarnings("serial")
-public class NoSecondaryStorageException extends IOException {
-	public NoSecondaryStorageException() { super(); }
-}
