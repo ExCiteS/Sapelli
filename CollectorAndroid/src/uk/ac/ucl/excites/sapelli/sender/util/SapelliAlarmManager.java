6e5e5c3452c9f1abb4bab4dd80d20716cb65168a
Michalis Vitos <info@michalisvitos.gr>
2014-12-19 13:24:50 +0000

    Added SapelliAlarmManager.java (Work in progress)
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

---
+++ CollectorAndroid/src/uk/ac/ucl/excites/sapelli/sender/util/SapelliAlarmManager.java
@@ -0,0 +1,114 @@
+package uk.ac.ucl.excites.sapelli.sender.util;
+
+import uk.ac.ucl.excites.sapelli.collector.db.ProjectStore;
+import uk.ac.ucl.excites.sapelli.collector.model.Project;
+import uk.ac.ucl.excites.sapelli.sender.BootReceiver;
+import uk.ac.ucl.excites.sapelli.sender.DataSenderService;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.SystemClock;
+
+/**
+ * @author Michalis Vitos
+ * 
+ */
+public class SapelliAlarmManager
+{
+	public static final String PROJECT_ID = "projectId";
+	public static final String PROJECT_HASH = "projectHash";
+
+	public SapelliAlarmManager()
+	{
+		// Nothing goes here
+	}
+
+	/**
+	 * Set up an Alarm for a project that calls the {@link DataSenderService}, initially after a minute and then every <code>intervalMillis</code>
+	 * 
+	 * @param context
+	 * @param intervalMillis
+	 * @param projectHashCode
+	 */
+	public static void setAlarm(Context context, int intervalMillis, int projectID)
+	{
+		setAlarm(context, 60 * 1000, intervalMillis, projectID);
+	}
+
+	/**
+	 * Set up an Alarm for a project that calls the {@link DataSenderService}, initially after <code>triggerAtMillis</code> and then every
+	 * <code>intervalMillis</code>
+	 * 
+	 * @param context
+	 * @param triggerDelay
+	 * @param intervalMillis
+	 * @param projectHashCode
+	 */
+	public static void setAlarm(Context context, int triggerDelay, int intervalMillis, int projectID)
+	{
+		// Create Alarm Manager
+		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+		// Setup the alarm to be triggered every intervalMillis
+		alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + triggerDelay, intervalMillis,
+				getAlarmIntent(context, projectID));
+
+		// Enable the Broadcast Receiver
+		enableBootReceiver(context, true);
+	}
+
+	public static void cancelAlarm(Context context, int projectID)
+	{
+		// Create Alarm Manager
+		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.cancel(getAlarmIntent(context, projectID));
+
+		// Check whether to cancel BootReceiver
+		checkBootReceiver(context);
+	}
+
+	private static PendingIntent getAlarmIntent(Context context, int projectID)
+	{
+		// Create the PendingIntent for the DataSenderService
+		Intent serviceIntent = new Intent(context, DataSenderService.class);
+		serviceIntent.putExtra(PROJECT_ID, projectID);
+		PendingIntent alarmIntent = PendingIntent.getService(context, projectID, serviceIntent, 0);
+		return alarmIntent;
+	}
+
+	/**
+	 * Checks whether there is any project with Sending activated and either disables or enables the boot receiver
+	 * 
+	 * @param context
+	 */
+	private static void checkBootReceiver(Context context)
+	{
+		boolean isSending = false;
+
+		// Get ProjectStore instance:
+		ProjectStore projectStore = null; // TODO This will crash, it needs a way of accessing the CollectoApp and get the projectstore
+		// ((CollectorApp) context.getApplication()).getProjectStore(null);
+
+		// For each of the projects that has sending enabled, set an Alarm
+		for(Project p : projectStore.retrieveProjects())
+		{
+			// TODO if (p.isSending())
+			isSending = false; // TODO change
+			break;
+		}
+
+		enableBootReceiver(context, isSending);
+	}
+
+	private static void enableBootReceiver(Context context, boolean enable)
+	{
+		ComponentName receiver = new ComponentName(context, BootReceiver.class);
+		PackageManager pm = context.getPackageManager();
+
+		final int newState = (enable) ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+		pm.setComponentEnabledSetting(receiver, newState, PackageManager.DONT_KILL_APP);
+	}
+}
