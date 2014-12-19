2cc688ba52161d8e5b460b36dc33252d68a17c01
Michalis Vitos <info@michalisvitos.gr>
2014-12-19 15:37:12 +0000

    Added BootReceiver class and enabled it in the Manifest (Work in progress)
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- CollectorAndroid/src/uk/ac/ucl/excites/sapelli/sender/BootReceiver.java
+++ CollectorAndroid/src/uk/ac/ucl/excites/sapelli/sender/BootReceiver.java
@@ -1,55 +1,26 @@
-/**
- * Sapelli data collection platform: http://sapelli.org
- * 
- * Copyright 2012-2014 University College London - ExCiteS group
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and 
- * limitations under the License.
- */
-
 package uk.ac.ucl.excites.sapelli.sender;
 
-import java.util.Calendar;
-
-import android.app.PendingIntent;
+import uk.ac.ucl.excites.sapelli.util.Debug;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 public class BootReceiver extends BroadcastReceiver
 {
-
 	@Override
-	public void onReceive(Context mContext, Intent mIntent)
+	public void onReceive(Context context, Intent intent)
 	{
 
-		PendingIntent mPendingIntent = null;
+		Debug.d("The device has been rebooted. The context is: " + context + " and the intent is: " + intent);
 
-		if(DataSenderPreferences.getSenderEnabled(mContext))
-		{
-			// TODO Re-enable the service at same point
-			// Intent service = new Intent(mContext, DataSenderService.class);
-			// mPendingIntent = PendingIntent.getService(mContext, 0, service, 0);
-		}
-
-		// Set up a calendar 2 minutes from now
-		Calendar mCalendar = Calendar.getInstance();
-		mCalendar.setTimeInMillis(System.currentTimeMillis());
-		mCalendar.add(Calendar.MINUTE, 2);
-
-		// TODO Re-enable the service at same point
-		// Schedule the alarm!
-		// AlarmManager mAlarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
-		// mAlarmManager.set(AlarmManager.RTC_WAKEUP, mCalendar.getTimeInMillis(), mPendingIntent);
+		// TODO Call a service from here for scheduling alarms
+		/*
+		 * if(intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) { // Check if projects require data transmission and set up alarms for the
+		 * DataSenderService ProjectStore projectStore = null; try { // Get ProjectStore instance: projectStore = CollectorApp.GetProjectStore(context);
+		 * 
+		 * // For each of the projects that has sending enabled, set an Alarm for(Project p : projectStore.retrieveProjects()) // TODO if (p.isSending()) //
+		 * TODO interval should be saved in project -> p.getSendingInterval() SapelliAlarmManager.setAlarm(context, 60 * 1000, p.getID()); } catch(Exception e)
+		 * { // TODO } finally { if(projectStore != null) projectStore.finalise(); } }
+		 */
 	}
-
 }
