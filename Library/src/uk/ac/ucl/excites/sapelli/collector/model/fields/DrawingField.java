7f59520ddae413a0b7af20dbac463473e774df20
benelliott <bengelliott@gmail.com>
2015-01-12 10:38:11 +0000

    Replaced old getFiles method with newer addFiles in DrawingField
    Signed-off-by: benelliott <bengelliott@gmail.com>

--- Library/src/uk/ac/ucl/excites/sapelli/collector/model/fields/DrawingField.java
+++ Library/src/uk/ac/ucl/excites/sapelli/collector/model/fields/DrawingField.java
@@ -1,8 +1,7 @@
 package uk.ac.ucl.excites.sapelli.collector.model.fields;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 import uk.ac.ucl.excites.sapelli.collector.io.FileStorageProvider;
 import uk.ac.ucl.excites.sapelli.collector.model.Field;
@@ -101,16 +100,15 @@ public class DrawingField extends MediaField
 	{
 		this.addDrawingImageRelativePath = addDrawingImageRelativePath;
 	}
-
+	
 	@Override
-	public List<File> getFiles(FileStorageProvider fileStorageProvider)
+	public void addFiles(Set<File> filesSet, FileStorageProvider fileStorageProvider)
 	{
-		List<File> paths = new ArrayList<File>();
-		CollectionUtils.addIgnoreNull(paths, fileStorageProvider.getProjectImageFile(form.project, captureButtonImageRelativePath));
-		CollectionUtils.addIgnoreNull(paths, fileStorageProvider.getProjectImageFile(form.project, discardButtonImageRelativePath));
-		CollectionUtils.addIgnoreNull(paths, fileStorageProvider.getProjectImageFile(form.project, addDrawingImageRelativePath));
-		CollectionUtils.addIgnoreNull(paths, fileStorageProvider.getProjectImageFile(form.project, canvasImageRelativePath));
-		return paths;
+		super.addFiles(filesSet, fileStorageProvider); // !!!
+		CollectionUtils.addIgnoreNull(filesSet, fileStorageProvider.getProjectImageFile(form.project, captureButtonImageRelativePath));
+		CollectionUtils.addIgnoreNull(filesSet, fileStorageProvider.getProjectImageFile(form.project, discardButtonImageRelativePath));
+		CollectionUtils.addIgnoreNull(filesSet, fileStorageProvider.getProjectImageFile(form.project, addDrawingImageRelativePath));
+		CollectionUtils.addIgnoreNull(filesSet, fileStorageProvider.getProjectImageFile(form.project, canvasImageRelativePath));
 	}
 	
 	/**
