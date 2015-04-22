4e4ae9cb6b2c75fc2e83b136f04cfbb4724f4774
Matthias Stevens <matthias.stevens@gmail.com>
2015-04-22 15:54:33 +0100

    Controller: added javadoc
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

--- Library/src/uk/ac/ucl/excites/sapelli/collector/control/Controller.java
+++ Library/src/uk/ac/ucl/excites/sapelli/collector/control/Controller.java
@@ -373,6 +373,9 @@ public abstract class Controller<CUI extends CollectorUI<?, ?>> implements Field
 		goTo(currFormSession.getCurrent(), leaveRule);
 	}
 	
+	/**
+	 * Re-enter current field with new arguments
+	 */
 	public void goToCurrent(FieldParameters arguments, LeaveRule leaveRule)
 	{
 		goTo(new FieldWithArguments(currFormSession.getCurrentField(), arguments), leaveRule);
