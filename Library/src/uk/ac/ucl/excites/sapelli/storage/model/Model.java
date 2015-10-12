9d15f95372e67984b5354867d6ff3f2cbcec60ec
Matthias Stevens <matthias.stevens@gmail.com>
2015-10-12 22:30:50 +0200

    Model: added toString() method
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

--- Library/src/uk/ac/ucl/excites/sapelli/storage/model/Model.java
+++ Library/src/uk/ac/ucl/excites/sapelli/storage/model/Model.java
@@ -201,7 +201,9 @@ public class Model implements Serializable
 	/**
 	 * Creates a new model
 	 * 
+	 * @param id
 	 * @param name
+	 * @param meta
 	 */
 	private Model(long id, String name, boolean meta)
 	{
@@ -353,6 +355,12 @@ public class Model implements Serializable
 					this.sealed == that.sealed;
 		}
 		return false;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return getClass().getSimpleName() + "_" + (name != null ? name + "_" : "") + "(ID=" + id + ")"; 
 	}
 	
 }
