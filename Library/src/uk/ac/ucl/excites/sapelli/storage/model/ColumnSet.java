385fbdf993a5d45a6b1de022ed46c5b835a54990
Matthias Stevens <matthias.stevens@gmail.com>
2015-10-13 16:10:23 +0200

    ColumnSet#toString() changed to be more like Model#toString()
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

--- Library/src/uk/ac/ucl/excites/sapelli/storage/model/ColumnSet.java
+++ Library/src/uk/ac/ucl/excites/sapelli/storage/model/ColumnSet.java
@@ -476,7 +476,7 @@ public class ColumnSet implements Serializable
 	@Override
 	public String toString()
 	{
-		return getClass().getSimpleName() + " " + name;
+		return getClass().getSimpleName() + "_" + name;
 	}
 	
 	/**
