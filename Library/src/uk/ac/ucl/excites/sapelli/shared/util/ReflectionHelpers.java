3235151dac3534cb1b63c62d76bcf8aa6d0b8159
Matthias Stevens <matthias.stevens@gmail.com>
2015-10-19 23:04:15 +0200

    ReflectionHelpers: deprecated setFinal() because it can't work on Android/Dalvik
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

--- Library/src/uk/ac/ucl/excites/sapelli/shared/util/ReflectionHelpers.java
+++ Library/src/uk/ac/ucl/excites/sapelli/shared/util/ReflectionHelpers.java
@@ -33,13 +33,36 @@ public final class ReflectionHelpers
 	/**
 	 * Changes the {@code final} modifier of the given field.
 	 * 
+	 * Tested and found working on Oracle JVM. Probably also works on Android ART, but *not* on Dalvik.
+	 * 
 	 * @param field
 	 * @param setFinal new state({@code true} field is made {@code final}; {@code false} field is made "non-{@code final}") 
 	 * @throws Exception
+	 * 
+	 * @see {@link Field#getModifiers()}
+	 * @see <a href="https://android.googlesource.com/platform/libcore/+/master/libart/src/main/java/java/lang/reflect/Field.java">ART implementation of Field</a>
+	 * @see <a href="https://android.googlesource.com/platform/libcore/+/9edf43dfc/libdvm/src/main/java/java/lang/reflect/Field.java">Dalvik implementation of Field</a>
+	 * 
+	 * @deprecated Doesn't work on Android/Dalvik
 	 */
 	static public void setFinal(Field field, boolean setFinal) throws Exception
 	{
-		Field modifiersField = Field.class.getDeclaredField("modifiers");
+		Field modifiersField = null;
+		try
+		{
+			modifiersField = Field.class.getDeclaredField("modifiers"); // JVM
+		}
+		catch(NoSuchFieldException nsfe)
+		{
+			try
+			{
+				modifiersField = Field.class.getDeclaredField("accessFlags"); // should work on Android ART (not on Dalvik!)
+			}
+			catch(NoSuchFieldException nsfe2)
+			{
+				throw new NoSuchFieldException("There is no field called \"modifiers\", nor one called \"accessFlags\"!");
+			}
+		}
 		modifiersField.setAccessible(true);
 		modifiersField.setInt(field, (setFinal ?	field.getModifiers() | Modifier.FINAL :
 													field.getModifiers() & ~Modifier.FINAL));
