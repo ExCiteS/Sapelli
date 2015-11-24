425cfd59d9f96a2181296b56f9d181cdc02ec783
Matthias Stevens <matthias.stevens@gmail.com>
2015-11-24 22:18:13 +0100

    New UnknownCorrespondentException
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

---
+++ Library/src/uk/ac/ucl/excites/sapelli/transmission/util/UnknownCorrespondentException.java
@@ -0,0 +1,42 @@
+/**
+ * Sapelli data collection platform: http://sapelli.org
+ * 
+ * Copyright 2012-2014 University College London - ExCiteS group
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package uk.ac.ucl.excites.sapelli.transmission.util;
+
+import uk.ac.ucl.excites.sapelli.shared.db.exceptions.DBException;
+
+/**
+ * @author mstevens
+ *
+ */
+public class UnknownCorrespondentException extends DBException
+{
+
+	private static final long serialVersionUID = 1L;
+	
+	public UnknownCorrespondentException()
+	{
+		this("Unknown correspondent!");
+	}
+	
+	public UnknownCorrespondentException(String message)
+	{
+		super(message);
+	}
+	
+}
