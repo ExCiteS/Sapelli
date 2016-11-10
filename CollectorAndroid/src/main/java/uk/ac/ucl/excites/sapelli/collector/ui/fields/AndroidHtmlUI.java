0971ed9956ba37e189a267752610b64c6fd48c5e
Michalis Vitos <info@michalisvitos.gr>
2016-11-10 16:57:26 +0000

    Automatically differentiate between online URLs and local files that are stored in the "resources" directory
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- CollectorAndroid/src/main/java/uk/ac/ucl/excites/sapelli/collector/ui/fields/AndroidHtmlUI.java
+++ CollectorAndroid/src/main/java/uk/ac/ucl/excites/sapelli/collector/ui/fields/AndroidHtmlUI.java
@@ -1,31 +1,34 @@
 /**
  * Sapelli data collection platform: http://sapelli.org
- * 
+ * <p>
  * Copyright 2012-2016 University College London - ExCiteS group
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and 
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
 package uk.ac.ucl.excites.sapelli.collector.ui.fields;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
+
+import org.apache.commons.validator.routines.UrlValidator;
+
+import java.util.Locale;
 
 import timber.log.Timber;
 import uk.ac.ucl.excites.sapelli.collector.R;
@@ -37,7 +40,6 @@ import uk.ac.ucl.excites.sapelli.storage.model.Record;
 
 /**
  * @author Michalis Vitos
- *
  */
 public class AndroidHtmlUI extends HtmlUI<View, CollectorView>
 {
@@ -53,34 +55,56 @@ public class AndroidHtmlUI extends HtmlUI<View, CollectorView>
 	{
 		if (backgroundView == null)
 		{
-			Context context = collectorUI.getContext();
+			final Context context = collectorUI.getContext();
+			final int COLOUR_WHITE = ContextCompat.getColor(context, R.color.white);
 			backgroundView = new RelativeLayout(context);
-			backgroundView.setBackgroundColor(ContextCompat.getColor(context, R.color.blue_sap));
+			backgroundView.setBackgroundColor(COLOUR_WHITE);
 
+			// Set parameters
 			RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
 			params.addRule(RelativeLayout.CENTER_IN_PARENT);
-
 
 			// Webview
 			WebView webView = new WebView(context);
 			webView.getSettings().setJavaScriptEnabled(true);
 			webView.setWebViewClient(new WebViewClient());
-			webView.setBackgroundColor(Color.argb(1, 0, 0, 0));
+			webView.setBackgroundColor(COLOUR_WHITE);
 			webView.getSettings().setDomStorageEnabled(true);
 			webView.getSettings().setDatabaseEnabled(true);
-			if (true)
-			{
-				webView.getSettings().setAppCacheEnabled(true);
-				webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-			}
-			//final String url = "file:///storage/emulated/0/Download/planted.html";
-			final String url = "file://" + controller.getFileStorageProvider().getProjectResFile(controller.getCurrentForm().getProject(), "planted.html");
-			webView.loadUrl(url);
-			Timber.d("Try to load: %s", url);
+			webView.getSettings().setAppCacheEnabled(true);
+			webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
 
-			// View
-			View view = new View(context);
-			view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimary));
+			//final String url = "file:///storage/emulated/0/Download/planted.html";
+			//final String url = "file://" + controller.getFileStorageProvider().getProjectResFile(controller.getCurrentForm().getProject(), "planted.html");
+			UrlValidator urlValidator = new UrlValidator();
+			String url = "";
+			try
+			{
+				if (urlValidator.isValid(field.getUrl()))
+				{
+					url = field.getUrl();
+				}
+				else
+				{
+					String localFile = controller.getFileStorageProvider()
+							.getProjectResFile(
+									controller.getCurrentForm().getProject(),
+									field.getUrl()
+							)
+							.toString();
+
+					// file://local_file
+					url = String.format(Locale.getDefault(), "file://%s", localFile);
+				}
+			}
+			catch (Exception e)
+			{
+				Timber.e(e, "Error in URL builder: ");
+			}
+
+			Timber.d("URL: %s", url);
+
+			webView.loadUrl(url);
 
 			backgroundView.addView(webView, params);
 		}
