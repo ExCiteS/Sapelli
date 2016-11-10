6c4e8de0425ba3a5147e5f14b06f1fdbeec080f1
Michalis Vitos <info@michalisvitos.gr>
2016-11-10 12:25:45 +0000

    WIP: Add HtmlField to support Html
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

---
+++ Library/src/main/java/uk/ac/ucl/excites/sapelli/collector/model/fields/HtmlField.java
@@ -0,0 +1,83 @@
+/**
+ * Sapelli data collection platform: http://sapelli.org
+ * <p>
+ * Copyright 2012-2016 University College London - ExCiteS group
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.ucl.excites.sapelli.collector.model.fields;
+
+import uk.ac.ucl.excites.sapelli.collector.control.FieldVisitor;
+import uk.ac.ucl.excites.sapelli.collector.model.Field;
+import uk.ac.ucl.excites.sapelli.collector.model.FieldParameters;
+import uk.ac.ucl.excites.sapelli.collector.model.Form;
+import uk.ac.ucl.excites.sapelli.collector.ui.CollectorUI;
+import uk.ac.ucl.excites.sapelli.collector.ui.fields.TextBoxUI;
+import uk.ac.ucl.excites.sapelli.storage.model.Column;
+
+/**
+ * A Field class representing an html page
+ *
+ * @author Michalis Vitos
+ */
+public class HtmlField extends Field
+{
+
+	static public final String ID_PREFIX = "html";
+
+	// Defaults
+
+
+	// Dynamics
+
+	/**
+	 * @param form
+	 * @param id      the id of the field, may be null (but not recommended)
+	 * @param caption the caption of the field, may be null (in which case there will be no text label above the textbox)
+	 */
+	public HtmlField(Form form, String id, String caption)
+	{
+		super(form,
+				GetID(id, form, ID_PREFIX, caption),
+				caption);
+	}
+
+	@Override
+	protected Column<?> createColumn(String name)
+	{
+		// TODO: 10/11/2016 This should return the correct column
+		return null;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see uk.ac.ucl.excites.sapelli.collector.model.Field#enter(uk.ac.ucl.excites.sapelli.collector.control.FieldVisitor, uk.ac.ucl.excites.sapelli.collector.model.FieldParameters, boolean)
+	 */
+	@Override
+	public boolean enter(FieldVisitor visitor, FieldParameters arguments, boolean withPage)
+	{
+		return visitor.enterHtmlField(this, arguments, withPage);
+	}
+
+	/* (non-Javadoc)
+	 * @see uk.ac.ucl.excites.sapelli.collector.model.Field#createUI(uk.ac.ucl.excites.sapelli.collector.ui.CollectorUI)
+	 */
+	@Override
+	public <V, UI extends CollectorUI<V, UI>> TextBoxUI<V, UI> createUI(UI collectorUI)
+	{
+		// TODO: 10/11/2016 Fix this
+		return null;
+		//		return collectorUI.createTextFieldUI(this);
+	}
+}
