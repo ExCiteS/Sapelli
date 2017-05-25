74256d70280bea9be2307cc0bd95519ed6cc53e7
Michalis Vitos <info@michalisvitos.gr>
2017-05-25 15:35:28 +0100

    Add log4j & logback and lombok support
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- CollectorPackager/src/main/java/uk/ac/ucl/excites/sapelli/packager/Packager.java
+++ CollectorPackager/src/main/java/uk/ac/ucl/excites/sapelli/packager/Packager.java
@@ -1,12 +1,33 @@
+/**
+ * Sapelli data collection platform: http://sapelli.org
+ * <p>
+ * Copyright 2012-2016 University College London - ExCiteS group
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.ac.ucl.excites.sapelli.packager;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by Michalis on 25/05/2017.
  */
+@Slf4j
 public class Packager
 {
 	public static void main(String[] args)
 	{
-
+		log.info("Packager!!!");
 	}
 }
