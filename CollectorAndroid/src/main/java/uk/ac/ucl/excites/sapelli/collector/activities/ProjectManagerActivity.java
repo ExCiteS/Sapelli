c36653e76db2c28c6f5711cbdb314434a04865ad
Michalis Vitos <info@michalisvitos.gr>
2017-11-28 11:14:54 +0000

    Add missing import
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- CollectorAndroid/src/main/java/uk/ac/ucl/excites/sapelli/collector/activities/ProjectManagerActivity.java
+++ CollectorAndroid/src/main/java/uk/ac/ucl/excites/sapelli/collector/activities/ProjectManagerActivity.java
@@ -40,6 +40,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.crashlytics.android.Crashlytics;
